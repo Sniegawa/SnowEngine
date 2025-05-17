ed3458a4dd9042d56784c9e606b30c702940c259
Sniegawa <xaffy25@gmail.com>
2025-05-17 13:51:21 +0200

    Got window to work

---
+++ SnowEngine/main.cpp
@@ -0,0 +1,61 @@
+﻿#include <iostream>
+
+
+#include "GL/glew.h"
+
+#include "GLFW/glfw3.h"
+
+#include "glm/glm.hpp"
+
+GLFWwindow* window;
+
+glm::vec2 ScreenSize = glm::vec2(1280, 720);
+
+int main()
+{
+
+	glewExperimental = true;
+	if (!glfwInit())
+	{
+		fprintf(stderr, "Failed to initialize GLFW\n");
+		return -1;
+	}
+	glfwWindowHint(GLFW_SAMPLES, 4);//Anti aliasing
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	window = glfwCreateWindow(ScreenSize.x, ScreenSize.y, "SnowEngine", NULL, NULL);
+	if (window == NULL)
+	{
+		fprintf(stderr, "Failed to create window");
+		glfwTerminate();
+		return -1;
+	}
+	glViewport(0, 0, ScreenSize.x, ScreenSize.y);
+	glfwMakeContextCurrent(window);
+	glewExperimental = true;
+	if (glewInit() != GLEW_OK)
+	{
+		fprintf(stderr, "Failed to initialize GLEW");
+		return -1;
+	}
+
+
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+	
+
+	glClearColor(0.0f, 1.0f, 0.5f, 1.0f);
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
+	}
+	
+	return 0;
+}
